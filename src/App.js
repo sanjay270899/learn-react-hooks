39f0f2027656f8e6d0d832673d0cf2c5e5f7a379
Sanjay Prajapati <sanjay270899@gmail.com>
2020-12-17 02:04:18 +0530

    Web Hosting Fixed.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Author from './components/Author';
 import Footer from './components/Footer';
 import Header from './components/Header';
